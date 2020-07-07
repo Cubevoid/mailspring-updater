84b254becb65eb3de20a4e4045e1866f476fa23a
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2020-07-07 17:40:17 -0400

    Support for Ubuntu/Debian
    Not sure if this actually works, I don't have a Debian-based install to test this on

--- update_mailspring.sh
+++ update_mailspring.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
+
+# RUN THIS SCRIPT AS SUDO
+
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+fi
+
 cd ~/Downloads
 rm mailspring-*
 echo "Removed old version..."
-sudo dnf remove mailspring -y
-echo "Downloading latest Mailspring release..."
-curl -s https://api.github.com/repos/Foundry376/Mailspring/releases/latest \
-  | grep browser_download_url.*rpm \
-  | cut -d : -f 2,3 \
-  | tr -d \" \
-  | wget -qi -
-echo "Installing new version..."
-sudo dnf install mailspring* -y
+
+if [ $OS == "Fedora" ]; then
+
+    dnf remove mailspring -y
+    echo "Downloading latest Mailspring release..."
+    curl -s https://api.github.com/repos/Foundry376/Mailspring/releases/latest \
+      | grep browser_download_url.*rpm \
+      | cut -d : -f 2,3 \
+      | tr -d \" \
+      | wget -qi -
+    echo "Installing new version..."
+    dnf install mailspring* -y
+
+elif [ $OS == "Ubuntu" -o $OS == "Debian" ]; then
+    
+    apt remove mailspring -y
+    echo "Downloading latest Mailspring release..."
+    curl -s https://api.github.com/repos/Foundry376/Mailspring/releases/latest \
+      | grep browser_download_url.*deb \
+      | cut -d : -f 2,3 \
+      | tr -d \" \
+      | wget -qi -
+    echo "Installing new version..."
+    apt install mailspring* -y
+fi
+
 rm mailspring-*
 echo "Removed junk files."
